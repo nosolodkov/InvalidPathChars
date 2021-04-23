448513861701b43d87573c2b827eb10de485d95f
Nikolay Osolodkov <nosolodkov@purelogic.ru>
2021-04-23 08:26:36 +0300

    Add project files.

---
+++ InvalidPathChars/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace InvalidPathChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var outFile = args.Length >= 1 ?
+                args[0] :
+                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "InvalidNames.txt");
+
+            // Attempt to open output file
+
+            using (var fs = new FileStream(outFile, FileMode.OpenOrCreate))
+            {
+                using (var writer = new StreamWriter(fs, System.Text.Encoding.UTF8))
+                {
+
+                    // Redirect standard output from the console to the output file.
+                    Console.SetOut(writer);
+
+                    Console.WriteLine(string.Join(" ", Path.GetInvalidFileNameChars()));
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine(string.Join(" ", Path.GetInvalidPathChars()));
+                }
+            }
+        }
+    }
+}
